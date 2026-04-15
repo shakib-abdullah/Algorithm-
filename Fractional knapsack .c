27d0ae989bf5a09ec0a3b30dbe718c7956d07a4e
shakib <shakibabdulllah500@gmail.com>
2026-04-15 15:37:28 +0600

    fractional knapsack code updated

--- Fractional knapsack .c
+++ Fractional knapsack .c
@@ -1,66 +1,64 @@
 #include <stdio.h>
 
-struct Item {
-    int value;
-    int weight;
-};
+int main() {
+    int n, W;
+    float value[20], weight[20], ratio[20], temp;
+    float totalValue = 0;
 
-void sortItems(struct Item arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            double r1 = (double)arr[j].value / arr[j].weight;
-            double r2 = (double)arr[j + 1].value / arr[j + 1].weight;
-            if (r1 < r2) {
-                struct Item temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    // METHOD 1: STATIC DECLARATION
+    n = 3;
+    W = 50;
+    float v[] = {60, 100, 120};
+    float w[] = {10, 20, 30};
+    for(int i=0; i<n; i++) {
+        value[i] = v[i];
+        weight[i] = w[i];
+    }
+
+    // METHOD 2: USER INPUT
+    printf("Item koyta? ");
+    scanf("%d", &n);
+    printf("Bag er capacity koto? ");
+    scanf("%d", &W);
+    for (int i = 0; i < n; i++) {
+        printf("Item %d er Value ar Weight dao: ", i + 1);
+        scanf("%f %f", &value[i], &weight[i]);
+    }
+
+
+    // 1. Proti kg er dam (Ratio) ber kora
+    for (int i = 0; i < n; i++) {
+        ratio[i] = value[i] / weight[i];
+    }
+
+    // 2. Ratio onujayi boro theke choto shajano (Sorting)
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (ratio[i] < ratio[j]) {
+                // Ratio swap
+                temp = ratio[j]; ratio[j] = ratio[i]; ratio[i] = temp;
+                // Sathe sathe weight o swap
+                temp = weight[j]; weight[j] = weight[i]; weight[i] = temp;
+                // Sathe sathe value o swap
+                temp = value[j]; value[j] = value[i]; value[i] = temp;
             }
         }
     }
-}
 
-
-
-double fractionalKnapsack(int W, struct Item arr[], int n) {
-    sortItems(arr, n);
-    int curWeight = 0;
-    double finalValue = 0.0;
-
+    // 3. Bag e bhora shuru
     for (int i = 0; i < n; i++) {
-        if (curWeight + arr[i].weight <= W) {
-            curWeight += arr[i].weight;
-            finalValue += arr[i].value;
+        if (weight[i] <= W) {
+            // Purota neya jabe
+            totalValue += value[i];
+            W -= weight[i];
         } else {
-            int remain = W - curWeight;
-            finalValue += arr[i].value * ((double)remain / arr[i].weight);
+            // Shudu kichu ongsho neya jabe
+            totalValue += value[i] * ((float)W / weight[i]);
             break;
         }
     }
-    return finalValue;
-}
 
-int main() {
-    int n, W;
-
-//    // METHOD 1: STATIC DECLARATION
-//    struct Item arr[] = {{60, 10}, {100, 20}, {120, 30}};
-//    n = sizeof(arr) / sizeof(arr[0]);
-//    W = 50;
-
-    // METHOD 2: USER INPUT
-    printf("Enter number of items: ");
-    scanf("%d", &n);
-    struct Item arr[n];
-    for (int i = 0; i < n; i++) {
-        printf("Enter value and weight for item %d: ", i + 1);
-        scanf("%d %d", &arr[i].value, &arr[i].weight);
-    }
-    printf("Enter capacity of knapsack: ");
-    scanf("%d", &W);
-
-
-    double maxValue = fractionalKnapsack(W, arr, n);
-    printf("Maximum value in Knapsack = %.2f\n", maxValue);
+    printf("Maximum labh hobe: %.2f\n", totalValue);
 
     return 0;
 }
